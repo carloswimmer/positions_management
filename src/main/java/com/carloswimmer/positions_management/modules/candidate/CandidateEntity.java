89643b41ac7b8445a0e9dc8e2a08047d8a6a4d79
Carlos Wimmer <coiote777@gmail.com>
2026-02-06 10:41:01 +0100

    Format error message

--- src/main/java/com/carloswimmer/positions_management/modules/candidate/CandidateEntity.java
+++ src/main/java/com/carloswimmer/positions_management/modules/candidate/CandidateEntity.java
@@ -14,13 +14,13 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "Username cannot have empty spaces")
+    @Pattern(regexp = "\\S+", message = "Username cannot have empty spaces")
     private String username;
 
     @Email(message = "Invalid email address")
     private String email;
 
-    @Length(min = 8, max = 100)
+    @Length(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     private String password;
     private String description;
     private String curriculum;
