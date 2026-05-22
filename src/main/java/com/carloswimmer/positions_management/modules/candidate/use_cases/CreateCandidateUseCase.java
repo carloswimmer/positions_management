edf4b5861f3751d84c67209cf42c33d9c0def2e0
Carlos Wimmer <coiote777@gmail.com>
2026-05-22 17:06:44 +0200

    Encode candidate password

--- src/main/java/com/carloswimmer/positions_management/modules/candidate/use_cases/CreateCandidateUseCase.java
+++ src/main/java/com/carloswimmer/positions_management/modules/candidate/use_cases/CreateCandidateUseCase.java
@@ -1,8 +1,7 @@
 package com.carloswimmer.positions_management.modules.candidate.use_cases;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.carloswimmer.positions_management.exceptions.ConflictException;
@@ -15,12 +14,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidate) {
         this.candidateRepository
-        .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
-        .ifPresent(entity -> {
-            throw new ConflictException("Candidate");
-        });
+                .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
+                .ifPresent(entity -> {
+                    throw new ConflictException("Candidate");
+                });
+
+        var password = passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(password);
 
         return this.candidateRepository.save(candidate);
     }
